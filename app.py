831c19cdb6febd4667043772b62c3f2bc3cbda02
Jit <jitnandi492@gmail.com>
2023-04-09 22:17:05 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
-import scipy
 
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
